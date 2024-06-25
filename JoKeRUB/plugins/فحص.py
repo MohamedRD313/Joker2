a88cb0195870af26464db5d0219f6e77d0ce810d
𝐌𝐨𝐡𝐚𝐦𝐞𝐝 𝐑𝐢𝐝𝐡𝐚 <hacker07940@gmail.com>
2024-06-25 22:54:25 +0300

    Update فحص.py

--- JoKeRUB/plugins/فحص.py
+++ JoKeRUB/plugins/فحص.py
@@ -45,7 +45,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "⿻┊‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ✔️صح[ سورس العقرب يشتغل ](t.me/jepthon)父**"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ✔️[ سورس العقرب يشتغل ](t.me/jepthon)父**"
     HuRe_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
     l313l_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = l313l_caption.format(
