4fe0dafa68ba5bcabe260b7b43d399efd8e11ae1
𝐌𝐨𝐡𝐚𝐦𝐞𝐝 𝐑𝐢𝐝𝐡𝐚 <hacker07940@gmail.com>
2024-06-26 15:34:55 +0300

    Update فحص.py

--- JoKeRUB/plugins/فحص.py
+++ JoKeRUB/plugins/فحص.py
@@ -45,7 +45,7 @@ async def amireallyalive(event):
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
     EMOJI = gvarstatus("ALIVE_EMOJI") or "✎┊‌‎"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ======================            𝗦𝗰𝗼𝗿𝗽𝗶𝗼 𝘄𝗼𝗿𝗸𝘀 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 ✅ **"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ╔========================╗            𝗦𝗰𝗼𝗿𝗽𝗶𝗼 𝘄𝗼𝗿𝗸𝘀 𝘀𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 ✅ **"
     HuRe_IMG = gvarstatus("ALIVE_PIC") or Config.A_PIC
     l313l_caption = gvarstatus("ALIVE_TEMPLATE") or temp
     caption = l313l_caption.format(
@@ -101,4 +101,4 @@ temp = """{ALIVE_TEXT}
 ‌‎**{EMOJI}‌‎‌‎𝐒𝐞𝐭𝐮𝐩 𝐃𝐚𝐭𝐞 | `{Tare5}`** ٫
 
 - 𝗚𝗼 𝗮𝗻𝗱 𝗲𝗻𝗷𝗼𝘆 😉
-======================="""
+╚=========================╝"""
