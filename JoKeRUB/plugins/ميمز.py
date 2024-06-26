131fdc0920bf14971612af12935a812247093493
𝐌𝐨𝐡𝐚𝐦𝐞𝐝 𝐑𝐢𝐝𝐡𝐚 <hacker07940@gmail.com>
2024-06-26 15:55:20 +0300

    Update ميمز.py

--- JoKeRUB/plugins/ميمز.py
+++ JoKeRUB/plugins/ميمز.py
@@ -1,4 +1,4 @@
-#all write Codes By Team Aljoker @jepthon
+#all write Codes By Team Aljoker @Scorpions_scorp
 #By Hussein @lMl10l
 import asyncio
 import random
@@ -30,7 +30,7 @@ async def _(event):
         except YouBlockedUserError:
             await event.edit("** اولا الغي حظر @SpamBot وحاول مجددا**")
             return
-        await event.edit(f"- {response.message.message}\n @jepthon")
+        await event.edit(f"- {response.message.message}\n @Scorpions_scorp")
 
 
 @l313l.on(admin_cmd(pattern="الاغنية ?(.*)"))
