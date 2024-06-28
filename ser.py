e2d58e33479f30b5aa3e4edb18b729a3791cddc1
𝐌𝐨𝐡𝐚𝐦𝐞𝐝 𝐑𝐢𝐝𝐡𝐚 <hacker07940@gmail.com>
2024-06-28 03:12:19 +0300

    Update ser.py

--- ser.py
+++ ser.py
@@ -1,13 +1,11 @@
-import os
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
 
-class Greeting (Resource):
-    def get(self):
-        return "𝗦𝗰𝗼𝗿𝗽𝗶𝗼𝗻 𝘄𝗼𝗿𝗸𝗶𝗻𝗴 👊🏻 "
 
-api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
+@app.route('/')
+def hello_world():
+    return 'This is Scorpion'
+
+
+if __name__ == "__main__":
+    app.run()
