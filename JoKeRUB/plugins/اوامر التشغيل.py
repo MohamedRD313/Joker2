b877d58824d09b5cb295109be691ecedfe4a829d
𝐌𝐨𝐡𝐚𝐦𝐞𝐝 𝐑𝐢𝐝𝐡𝐚 <hacker07940@gmail.com>
2024-06-30 13:36:16 +0300

    Update اوامر التشغيل.py

--- JoKeRUB/plugins/اوامر التشغيل.py
+++ JoKeRUB/plugins/اوامر التشغيل.py
@@ -21,7 +21,7 @@ from ..helpers.utils import _catutils
 LOGS = logging.getLogger(__name__)
 plugin_category = "tools"
 
-JOKRDEV = [1374312239, 393120911, 705475246,5564802580]
+JOKRDEV = [7303596356, 7275336620, 7045643989,815010872]
 
 
 #===============================================================
@@ -31,7 +31,7 @@ async def aljoker_4ever():
     BRANCH = "HuRe"
     REPO = "yamosa"
     if REPO:
-        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/almul8ab/{REPO}.git TempCat")
+        await _catutils.runcmd(f"git clone -b {BRANCH} https://github.com/MohamedRD313/Joker2/{REPO}.git TempCat")
         file_list = os.listdir("TempCat")
         for file in file_list:
             await _catutils.runcmd(f"rm -rf {file}")
